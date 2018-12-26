3d7fc174f9f9fbe8ae1ce8d5e991fabd4d59311f
Yashu Mittal <mittalyashu77@gmail.com>
2018-12-26 12:46:27 +0530

    Remove stylesheet link tag from head

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import '~/assets/css/style.scss'
 import DefaultLayout from '~/layouts/Default.vue'
 
-export default function(Vue, { head }) {
+export default function(Vue) {
 	Vue.component('Layout', DefaultLayout)
-
-	head.link.push({
-		rel: 'stylesheet',
-		href: '/assets/css/style.css'
-	})
 }
