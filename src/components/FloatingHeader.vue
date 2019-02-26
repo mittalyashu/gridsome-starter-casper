7f0846abf875cfbf1202117b8643937fb59038a8
Yashu Mittal <mittalyashu77@gmail.com>
2019-02-26 21:13:36 +0530

    Create FloatingHeader component

---
+++ src/components/FloatingHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="floating-header">
+    <div class="floating-header-logo">
+      <a href="/">
+        <img v-if="Admin.site.logo" :src="Admin.site.logo" :alt="Admin.site.title + ' icon'" />
+        <span>{{ Admin.site.title }}</span>
+      </a>
+    </div>
+    <span class="floating-header-divider">&mdash;</span>
+    <div class="floating-header-title">{{ title }}</div>
+    <div class="floating-header-share">
+      <div class="floating-header-share-label">Share this <PointerIcon/></div>
+      <!-- TODO Add Twitter share link -->
+      <a class="floating-header-share-tw">
+        <TwitterIcon/>
+      </a>
+      <!-- TODO Add Facebook share link -->
+      <a class="floating-header-share-fb">
+        <FacebookIcon/>
+      </a>
+    </div>
+    <progress id="reading-progress" class="progress" value="0">
+      <div class="progress-container">
+        <span class="progress-bar"></span>
+      </div>
+    </progress>
+  </div>
+</template>
+
+<script>
+  import Admin from "../../data/admin.yml"
+  import PointerIcon from './icons/Pointer';
+  import FacebookIcon from './icons/Facebook'
+	import TwitterIcon from './icons/Twitter'
+
+  export default {
+    components: {
+      PointerIcon,
+      FacebookIcon,
+      TwitterIcon
+    },
+    comments: {
+      Admin() {
+        return Admin
+      }
+    }
+  }
+</script>
