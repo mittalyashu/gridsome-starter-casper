dfeb1821d1872a790337bb9208073bc273cdfa5d
Yashu Mittal <mittalyashu77@gmail.com>
2019-02-26 18:50:45 +0530

    Improve the about page

---
+++ src/layouts/Page.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+    <header class="site-header outer">
+      <div class="inner">
+        <Navbar :logo="true"/>
+      </div>
+    </header>
+    <main id="site-main" class="site-main outer">
+      <div class="inner">
+        <slot/>
+      </div>
+    </main>
+  </Layout>
+</template>
+
+<script>
+import Navbar from "../components/Navbar";
+
+export default {
+  metaInfo() {
+    return {
+      bodyAttrs: {
+        class: `page-template`
+      }
+    };
+  },
+  components: {
+    Navbar
+  }
+};
+</script>
+
---
+++ src/pages/About.vue
@@ -0,0 +1,51 @@
+<template>
+  <Page>
+    <article class="post-full post page" :class="pageClass">
+
+      <header class="post-full-header">
+        <h1 class="post-full-title">About us</h1>
+      </header>
+
+      <figure v-if="image" class="post-full-image">
+        <g-image v-bind="image" src="" alt="" />
+      </figure>
+
+      <section class="post-full-content">
+        <div class="post-content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.
+        </div>
+      </section>
+
+    </article>
+  </Page>
+</template>
+
+<script>
+import Page from "../layouts/Page";
+
+export default {
+  metaInfo() {
+    return {
+      title: 'About',
+      bodyAttrs: {
+        class: `page-about`
+      }
+    };
+  },
+  data() {
+    return {
+      image: ''
+    }
+  },
+  computed: {
+    pageClass() {
+      if (this.image === '') {
+        return 'no-image'
+      }
+    }
+  },
+  components: {
+    Page
+  }
+};
+</script>
