0be8115ad1452f69085004cf03882488d46df58b
Yashu Mittal <mittalyashu77@gmail.com>
2019-02-26 17:31:09 +0530

    Create author and tag references

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,20 +1,33 @@
-// This is where project configuration and installed plugin options are located. 
-// Learn more: https://gridsome.org/docs/config
-
 module.exports = {
-  siteName: 'Gridsome Casper',
-  siteUrl: 'https://www.gridsome.org',
-  siteDescription: 'The professional publishing platform',
-  titleTemplate: `%s - Gridsome`,
-  
-  plugins: [
-    {
+	siteName: 'Gridsome Casper',
+	siteUrl: 'https://www.gridsome.org',
+	siteDescription: 'The professional publishing platform',
+	titleTemplate: `%s - Gridsome`,
+
+	plugins: [
+		{
 			use: '@gridsome/source-filesystem',
 			options: {
 				path: 'blog/*.md',
 				typeName: 'Post',
 				route: '/:slug',
+				refs: {
+					author: 'Author',
+					tags: {
+						typeName: 'Tag',
+						route: '/tag/:title',
+						create: true
+					}
+				}
+			}
+		},
+		{
+			use: '@gridsome/source-filesystem',
+			options: {
+				path: 'data/author/*.md',
+				typeName: 'Author',
+				route: '/author/:id'
 			}
 		}
-  ]
+	]
 }
